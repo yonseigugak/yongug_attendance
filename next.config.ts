c8af2c322b1f32387ba9f6c607d9f28aed9fd716
yonseigugak <yongugensemble@gmail.com>
2026-03-04 15:51:30 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // 앱의 모든 경로에 대해 아래 보안 헤더를 적용함
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
